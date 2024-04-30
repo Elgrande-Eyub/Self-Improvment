afd7aa38dd4844cf6d2d2c36ce24cc5456c646d8
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2024-04-30 16:02:03 +0100

    test version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Self Improvment</title>
+        <title>Self Improvments</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
